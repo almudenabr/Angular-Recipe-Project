aca0291ce7dd278759cea48607edadb7f01c5cac
Almudena Blanco <ablanco@sorint.it>
2022-05-12 23:51:39 +0200

    feat: added data using string interpolation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'The name of the recipe',
+      'This is the description of the recipe',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fpxmMH_E-AiBsmYLqVvCmLR3TpclpTt1aw&usqp=CAU'
+    ),
+    new Recipe(
+      'The name of the recipe',
+      'This is the description of the recipe',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fpxmMH_E-AiBsmYLqVvCmLR3TpclpTt1aw&usqp=CAU'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
