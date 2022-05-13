e5c52896d504dc9e25530bdacf75bb198c9fef7d
Almudena Blanco <ablanco@sorint.it>
2022-05-13 17:49:39 +0200

    feat: added template to recipe-item.component and passing data with ptoperty binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,14 +9,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'The name of the recipe',
-      'This is the description of the recipe',
+      'The name of the recipe1',
+      'This is the description of the recipe1',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fpxmMH_E-AiBsmYLqVvCmLR3TpclpTt1aw&usqp=CAU'
     ),
     new Recipe(
-      'The name of the recipe',
-      'This is the description of the recipe',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fpxmMH_E-AiBsmYLqVvCmLR3TpclpTt1aw&usqp=CAU'
+      'The name of the recipe2',
+      'This is the description of the recipe2',
+      'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
     ),
   ];
 
