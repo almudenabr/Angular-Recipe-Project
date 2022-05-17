d0d512eb3dc6428dde91c655e07b3857393bbeda
Almudena Blanco <ablanco@sorint.it>
2022-05-17 17:43:27 +0200

    feat: pass data with event and prop binding (click on recipe and display it in another component)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -9,7 +9,13 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeElement: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
