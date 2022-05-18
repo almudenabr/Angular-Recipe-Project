55df0b60b3080c616fb9ad4df2c8a618f39892d4
Almudena Blanco <ablanco@sorint.it>
2022-05-18 18:08:06 +0200

    feat: close dropdown clicking anywhere in the page

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,12 +1,31 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
+
+//toggles the dropdown btn only clicking on that btn
+
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen = false;
+
+//   @HostListener('click') toggleOpen() {
+//     this.isOpen = !this.isOpen;
+//   }
+// }
+
+//toggles the dropdown btn cliking anywhere in the page
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
